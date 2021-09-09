2ce8833f597ded4951bff60cd1a19935de48398b
abelyo69 <abelyo69@yahoo.com>
2021-09-09 16:17:09 +0000

    hiden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3.4
+#!/usr/bin/python3
+import hidden_4
 if __name__ == "__main__":
-    from hidden_4 import *
-    for s in dir():
-        if s[:2] != "__":
-            print("{}".format(s))
+    for i in dir(hidden_4):
+        if i[0:2] != "__":
+            print("{:s}".format(i))
