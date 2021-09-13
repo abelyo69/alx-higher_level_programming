28a4c5c931cf39796c2571789b3533c2d1b7f275
abelyo69 <abelyo69@yahoo.com>
2021-09-13 12:48:58 +0000

    no letter c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,9 @@
 
 
 def new_in_list(my_list, idx, element):
-    new_list = my_list[:]
-    if idx >= 0 and idx < len(new_list):
-        new_list[idx] = element
-    return new_list
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
+
+    copy = [x for x in my_list]
+    copy[idx] = element
+    return (copy)
